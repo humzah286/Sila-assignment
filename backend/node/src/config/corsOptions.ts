045e28d0f301656a1885cf2c03d2e86f064ed7a8
humzah286 <humzah286@gmail.com>
2024-02-19 07:49:04 +0500

    Backend redundant code removal

--- backend/node/src/config/corsOptions.ts
+++ backend/node/src/config/corsOptions.ts
@@ -1,24 +1,8 @@
-const allowedOrigins: string[] = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
 
-interface CorsOptions {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => void;
-  optionsSuccessStatus: number;
-}
-
-// const corsOptions: CorsOptions = {
-//     origin: (origin, callback) => {
-//         if (allowedOrigins.includes(origin ? origin: "") || !origin) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     optionsSuccessStatus: 200
-// }
 
 const corsOptions = {
-    origin: true, // reflect the request origin, as opposed to `'*'`
-    credentials: true, // required to pass
+    origin: true, 
+    credentials: true, 
   };
 
 export default corsOptions;
--- backend/node/src/middleware/authJWT.ts
+++ backend/node/src/middleware/authJWT.ts
@@ -48,9 +48,7 @@ const refreshJWT = (req: Request, res: Response) => {
         process.env.REFRESH_TOKEN_SECRET || "refresh-token"
     );
 
-    console.log("decoded : ", decoded)
     if (!(typeof decoded === 'object' && 'email' in decoded)) {
-        console.log("returning false because of missing email in decoded object")
         return false
     }
 
@@ -81,27 +79,20 @@ const refreshJWT = (req: Request, res: Response) => {
 
 const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
 
-    // console.log("req.cookies : ", req.cookies)
-    // console.log("verifyJWT middleware : " + req.cookies.access_token + " : " + req.cookies.refresh_token )
 
     let user = verifyAccessJWT(req);
 
-    // console.log("from verifyAccessToken : ", user)
 
     if (!user) {
 
         user = verifyRefreshJWT(req);
 
-        console.log("from verifyRefreshToken : ", user)
 
         if (!user) {
-            console.log("returning invalid JWT tokens")
             return res.status(400).json({ status: "Error", message: "invalid JWT tokens" });
         }
         else {
             let response = refreshJWT(req, res);
-
-            console.log("from refreshJWT : ", response)
 
             if (!response) {
                 return res.status(400).json({ status: "Error", message: "invalid JWT tokens" });
@@ -112,14 +103,11 @@ const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
         }
     }
 
-    console.log("user after verfication : ", user)
-
     if (!user || !(typeof user === 'object' && 'email' in user)) {
         return res.status(400).json({ status: "Error", message: "invalid JWT tokens" });
     }
 
     prisma.user.findUnique({ where: { email: user.email } }).then((user: any) => {
-        console.log("user from db : ", user)
         if (!user) {
             return res.status(400).json({ status: "Error", message: "invalid JWT tokens" });
         }
